554e0442e13d664dabf4261bdee80bd276e2289c
benoit charret <benoit.charret@axioconsulting.fr>
2014-06-11 23:13:18 +0200

    rename variable

--- src/main/haproxy/start_haproxy.sh
+++ src/main/haproxy/start_haproxy.sh
@@ -3,7 +3,7 @@
 PIDFILE="/var/run/haproxy.pid"
 UP_COUNT=${UPSTREAMS:-1}
 
-UP_STANZAS="listen session-webapp :80"$'\n'
+ADD_CONFIG="listen session-webapp :80"$'\n'
 
 HAPROXY="/etc/haproxy"
 CONFIG="haproxy.cfg"
@@ -15,11 +15,11 @@ while [ $index -lt $UP_COUNT ]; do
 #for index in {1..$UP_COUNT}; do
   let $((index++))
   IP_ADDR=`eval echo '$'APP${index}_PORT_8080_TCP_ADDR`
-  UP_STANZAS="${UP_STANZAS}"$'\n'"server tomcat${index} ${IP_ADDR}:8080 check"
+  ADD_CONFIG="${ADD_CONFIG}"$'\n'"server tomcat${index} ${IP_ADDR}:8080 check"
 done
 
-UP_STANZAS="${UP_STANZAS}"$'\n'"stats enable"$'\n'"stats uri /stats"
+ADD_CONFIG="${ADD_CONFIG}"$'\n'"stats enable"$'\n'"stats uri /stats"
 
-echo "${UP_STANZAS}" >> "$CONFIG"
+echo "${ADD_CONFIG}" >> "$CONFIG"
 
 haproxy -f /etc/haproxy/haproxy.cfg -p "$PIDFILE"
